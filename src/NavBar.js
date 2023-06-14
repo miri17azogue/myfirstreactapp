e485ddff8818a7381ac18cd2280437ce88cd742d
miri17azogue <miriamazogue561@gmail.com>
2023-06-14 10:33:36 -0500

    cambio nombre

--- src/NavBar.js
+++ src/NavBar.js
@@ -11,7 +11,7 @@ function NavBar() {
       <nav className='navbar'>
         <div className='nav-container'>
           <NavLink exact to='/' className='nav-logo'>
-            Miriam | My Web
+            Miriam | My Web React
             <i className='fas fa-code'></i>
           </NavLink>
 
